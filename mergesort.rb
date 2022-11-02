445e530e1bbf546e09385fca72ded25abba9971f
brickwall <sk292207@gmail.com>
2022-11-02 18:30:35 +0530

    initial prototype complete
    while was used instead of until initially

---
+++ mergesort.rb
@@ -0,0 +1,22 @@
+require 'pry-byebug'
+
+def merge(array)
+    return array if(array.length == 1 || array.length == 0)
+    leftarray = merge(array.slice!(0,array.length/2))
+    rightarray = merge(array)
+
+    newarray = []
+    until(leftarray.empty? || rightarray.empty?) do
+        # binding.pry
+        if(leftarray[0] > rightarray[0])
+            newarray.push(leftarray[0])
+            leftarray.delete_at(0)
+        else
+            newarray.push(rightarray[0])
+            rightarray.delete_at(0)
+        end
+    end
+    return newarray+leftarray+rightarray
+end
+
+p merge([8,7,3,5,34,3,2,4,6,2])
